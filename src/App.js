da989357ffc2321ff937907310f28af0c205e727
Victor Tang <victortang1992@gmail.com>
2017-05-29 16:30:07 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { version } from 'inferno';
+import Component from 'inferno-component';
+import Logo from './logo';
+import './App.css';
+
+class App extends Component {
+  render() {
+
+    const store = this.context.store;
+    const state = store.getState();
+    const onClick = e => {
+        e.preventDefault();
+        store.dispatch({
+            type: 'CHANGE_NAME',
+            name: 'Jerry'
+        });
+    };
+
+    return (
+      <div className="App">
+        <div className="App-header">
+          <Logo width="80" height="80" />
+          <h2>{`Welcome to Inferno ${version}, `+state.name}</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code onClick={ onClick }>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { render } from 'inferno';
+import { Provider } from 'inferno-redux'; // builds of <Component> in inferno-component
+import { createStore } from 'redux';
+import App from './App';
+import './index.css';
+
+var initialstate = {
+    name: 'Tom'
+};
+
+const reducer = function(state = initialstate, action) {
+  switch (action.type) {
+    case 'CHANGE_NAME':    
+      return {
+        name: action.name
+      }
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+
+render(<Provider store={ store }><App /></Provider>, document.getElementById('app'));
