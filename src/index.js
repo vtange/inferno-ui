3667223b23242b5d85042e01d2972fa7afe401e4
Victor Tang <victortang1992@gmail.com>
2017-06-06 11:52:42 -0700

    split actions, reducer with index

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ import { createStore } from 'redux';
 import App from './App';        //User interface
 //import GameEngine from './Engine' // <Provider store={ store }><App /><GameEngine /></Provider>, App overlays over GameEngine, controls control either App or Game
 import './styling/index.css';
+import { reducer } from './actions.js';
 
-var initialstate = {
-    name: 'Tom'
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':    
-      return {
-        name: action.name
-      }
-    default:
-      return initialstate;
-  }
-}
 const store = createStore(reducer);
 
 render(<Provider store={ store }><App /></Provider>, document.getElementById('app'));
